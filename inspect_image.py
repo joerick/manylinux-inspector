c931db3be397450a0d1d4bbeccfac6063930b509
Joe Rickerby <joerick@mac.com>
2023-02-18 13:31:18 +0000

    Add functionality to output installed versions

--- inspect_image.py
+++ inspect_image.py
@@ -9,6 +9,14 @@ from oci_container import OCIContainer
 
 ContainerPath = PurePosixPath
 
+
+def re_extract(regex: str, input: str):
+    match = re.search(regex, input)
+    if not match:
+        return None
+    return match.group(1)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("image", help="Image to inspect")
@@ -16,19 +24,64 @@ def main():
 
     versions = {}
     with OCIContainer(image=args.image) as container:
-        auditwheel_output = container.call(["auditwheel", "--version"], capture_output=True)
-        versions["auditwheel"] = re.search(r"auditwheel (\d+\.\d+\.\d+)", auditwheel_output).group(1)
+        pipx_list_output = container.call(["pipx", "list", "--short"], capture_output=True).strip().splitlines()
+        for line in pipx_list_output:
+            name, _, version = line.partition(' ')
+            versions[name] = version
 
         pythons = container.glob(ContainerPath("/opt/python/"), "*/bin/python")
-        print (pythons)
+
+        versions["pipx"] = container.call(["pipx", "--version"], capture_output=True).strip()
+        
+        versions["pythons"] = {}
         for python_path in pythons:
-            inspect_python(container, python_path)
+            python_identifier = re_extract(
+                r"/opt/python/(.*)/bin/python", str(python_path)
+            )
+            assert python_identifier
+            versions["pythons"][python_identifier] = inspect_python(
+                container, python_path, python_identifier
+            )
 
-    
-    print(json.dumps(versions))
+    print(json.dumps(versions, indent=2))
 
-def inspect_python(container: OCIContainer, python_path: ContainerPath):
-    container.call([python_path, "-c", "import auditwheel; print(auditwheel)"], capture_output=True)
+
+def inspect_python(
+    container: OCIContainer, python_path: ContainerPath, python_identifier: str
+):
+    versions: dict[str, str] = {}
+    is_pypy = "pypy" in python_identifier
+    if not is_pypy:
+        versions["python"] = re_extract(
+            r"Python (\S+)",
+            container.call([python_path, "--version"], capture_output=True),
+        )
+    else:
+        versions["python"] = re_extract(
+            r"PyPy (\S+)",
+            container.call([python_path, "--version"], capture_output=True),
+        )
+
+    versions["setuptools"] = container.call(
+        [python_path, "-c", "import setuptools; print(setuptools.__version__)"],
+        capture_output=True,
+    ).strip()
+
+    pip_version_output = container.call(
+        [python_path, "-m", "pip", "--version"], capture_output=True
+    )
+    versions["pip"] = re_extract(r"pip (\S+)", pip_version_output)
+
+    pip_freeze_output = container.call(
+        [python_path, "-m", "pip", "freeze"], capture_output=True
+    ).strip().splitlines()
+
+    for line in pip_freeze_output:
+        name, _, version = line.partition('==')
+        versions[name] = version
+
+    return versions
+
 
 if __name__ == "__main__":
     main()
